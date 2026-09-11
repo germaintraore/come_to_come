9b203a9b5fa41c33042c88a11192709fe31419f6
TRAORE GERMAIN <germain.traore.sng@gmail.com>
2026-09-11 14:44:44 +0000

    apres modification correct des presentations puis supprimer du placement du bouton creer devoir

--- config/settings.py
+++ config/settings.py
@@ -131,6 +131,7 @@ AUTH_PASSWORD_VALIDATORS = [
     {'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator'},
 ]
 
+DEFAULT_CHARSET='utf-8'
 LANGUAGE_CODE = 'fr-fr'
 TIME_ZONE = 'Africa/Ouagadougou'
 USE_I18N = True
